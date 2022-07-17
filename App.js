8521a7d130c35d65e6a5edaf733391f984448116
SammmE <101365537+SammmE@users.noreply.github.com>
2022-07-17 17:43:35 -0400

    added screens

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import DailyPic from "./screens/DailyPic";
+import Spacecraft from "./screens/Spacecraft";
+import StarMap from "./screens/StarMap";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                intialRouteName="Home"
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+                <Stack.Screen
+                    name="Daily Pic"
+                    component={DailyPic}
+                ></Stack.Screen>
+                <Stack.Screen
+                    name="Spacecrafts"
+                    component={Spacecraft}
+                ></Stack.Screen>
+                <Stack.Screen
+                    name="Star Map"
+                    component={StarMap}
+                ></Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Daly pic screen</Text>
+            <Text>Home Screen</Text>
             <StatusBar style="auto" />
         </View>
     );
